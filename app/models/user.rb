353340c4c47aa191ea899f1149714b5d5efc2909
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-09 20:46:01 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- with_options presence: true do
-  validates :nickname, uniqueness: true
-  validates :birthday 
-end
-
- with_options presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
-   validates :last_name
-   validates :first_name
+  with_options presence: true do
+    validates :nickname, uniqueness: true
+    validates :birthday
   end
 
- with_options presence: true,format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
-  validates :last_name_kana
-  validates :first_name_kana
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :last_name
+    validates :first_name
   end
-  
 
- PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
- validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end
