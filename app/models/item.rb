235d589709f142a046a2e45b23e07bec556f8d58
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-11 21:39:08 +0900

    ActiveHashの導入とプルダウンの項目の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :delivery_time
+  belongs_to :shipping_time
+  belongs_to :condition
+
 end
