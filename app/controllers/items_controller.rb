33e8e099abb92b1a2aeb55666598dd788f1780c9
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-13 00:34:35 +0900

    ストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:item_name,:description,:price,:area_id,:category_id,:delivery_time_id,:shipping_charge_id,:condition_id).merge(user_id: current_user.id)
   end
   
 end
