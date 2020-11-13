f4b4e92371d55df76872b8c6bf7f69a92c14dd09
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-14 08:17:26 +0900

    テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name              { Faker::Lorem.words }
+    description                 { Faker::Lorem.sentence }
+    price              { Faker::Number.between(from: 300, to: 9999999)}
+    category_id  {Faker::Number.between(from: 2, to: 6)}
+    condition_id  {Faker::Number.between(from: 2, to: 6)}
+    shipping_charge_id  {Faker::Number.between(from: 2, to: 3)}
+    area_id   {Faker::Number.between(from: 2, to: 40)}
+    delivery_time_id {Faker::Number.between(from: 2, to: 4)}
+    # association :user と記述しても関連付けがうまくいかず、以下のように記述したところuserが生成できたのですが、association :userでエラーになってしまう原因はありますでしょうか？
+
+    after(:build) do |item|
+      item.user = FactoryBot.create(:user)
+      item.image.attach(io: File.open('public/camera.png'), filename: 'test_image.png')
+    end
   end
 end
