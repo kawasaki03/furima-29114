194100fc4a47062e7c20acd97171d4eb7d09fdf7
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-17 01:31:05 +0900

    編集ページへの遷移の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:show,:edit,:update]
+  before_action :set_item, only: [:show,:edit,:update,:ensure_correct_user]
+  before_action :ensure_correct_user, only: [:edit,:update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -35,6 +36,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def ensure_correct_user
+    unless @item.user == current_user
+      redirect_to root_path
+    end
   end
 
   private
