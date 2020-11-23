476c37a766a7234ab1a3f45320381516ed60aa71
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-23 13:37:08 +0900

    バリデーションの変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,22 +1,20 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :user,:item,:postal_code,:area_id,:city,:house_number,:building_name,:phone_number,:purchase_record
-
+  attr_accessor :user_id,:item_id,:postal_code,:area_id,:city,:house_number,:building_name,:phone_number
 
 with_options presence: true do
-  validates :user
-  validates :item
+  validates :user_id
+  validates :item_id
   validates :postal_code,format: {with:/\A[0-9]{3}-[0-9]{4}\z/,message: "is invalid. Include hyphen(-)"}
-  validates :area_id, numericality: {other_than:0, message:"can't be blank"}
+  validates :area_id, numericality: {other_than:1, message:"can't be blank"}
   validates :city
   validates :house_number
   validates :phone_number,format: {with:  /\A\d{10,11}\z/}
-  validates :purchase_record
 end
 
 def save 
-  PurchaseRecord.create(user: user, item: item)
-  Address.create(postal_code: postal_code, area_id: area_id,city: city, house_number: house_number, phone_number: phone_number, purchase_record: purchase_record)
+  purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+  Address.create(postal_code: postal_code, area_id: area_id,city: city, house_number: house_number, building_name: building_name,phone_number: phone_number, purchase_record_id: purchase_record.id)
 end
 
 end
