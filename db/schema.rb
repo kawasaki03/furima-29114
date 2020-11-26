38daa3b8a48d90bd4d4ad20ee68edd7233fcd26f
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-26 23:18:47 +0900

    マイグレーションファイル名の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_133309) do
+ActiveRecord::Schema.define(version: 2020_11_21_133400) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
