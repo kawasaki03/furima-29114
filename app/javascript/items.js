30120b7eed8309b099f162d0b1388f5ffd929372
kawasaki03 <h.kawasaki03@gmail.com>
2020-11-30 23:13:34 +0900

    プレビュー機能の実装

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -10,4 +10,38 @@ function item(){
   })
 }
 
+if (document.URL.match(/new/) || document.URL.match(/edit/)){
+document.addEventListener('DOMContentLoaded', function(){
+  const imagePreview = document.getElementById("image-preview");
+
+  const createImageHtml = (blob) =>{
+    
+    const imageElement = document.createElement('div');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src',blob)
+    imageElement.setAttribute("class","image")
+    
+    imageElement.appendChild(blobImage);
+    imagePreview.appendChild(imageElement);
+  };
+
+
+document.getElementById("item-image").addEventListener('change', function(e){
+  const imageContent = document.querySelector('img');
+  if (imageContent){
+    imageContent.remove();
+  }
+       
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHtml(blob);
+  
+   });
+  });
+  };
+
+
+
 window.addEventListener("load",item)
+
+
