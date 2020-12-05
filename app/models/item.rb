16f7efc699b176b15127565450e916e62e2fd637
kawasaki03 <h.kawasaki03@gmail.com>
2020-12-05 20:35:03 +0900

    コメントモデルの作成とアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one :purchase_record
+  has_many :comments
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
@@ -9,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_time
   belongs_to :shipping_charge
   belongs_to :condition
+  
 
   validates :item_name, :description, :image, presence: true
   validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
