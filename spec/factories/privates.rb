5c0915a9402ab3e5414765ed3415f2281044676a
kawasaki03 <h.kawasaki03@gmail.com>
2020-12-23 19:32:16 +0900

    テストコードの記述

--- spec/factories/privates.rb
+++ spec/factories/privates.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :private do
-    
+    first_name            { Gimei.name.first.kanji }
+    last_name             { Gimei.name.last.kanji }
+    first_name_kana       { Gimei.name.first.katakana }
+    last_name_kana        { Gimei.name.last.katakana }
+    birthday              { Faker::Date.in_date_period }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,5 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { 'a' + '1' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
-    first_name            { Gimei.name.first.kanji }
-    last_name             { Gimei.name.last.kanji }
-    first_name_kana       { Gimei.name.first.katakana }
-    last_name_kana        { Gimei.name.last.katakana }
-    birthday              { Faker::Date.in_date_period }
   end
 end
